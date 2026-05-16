01c33e94e46e0fee685ae6e280dfbc369949d5e0
HP <doudedjimensah16@gmail.com>
2026-05-16 14:06:04 +0100

    modificat

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -19,6 +19,21 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        \Illuminate\Support\Facades\View::composer(['layouts.dokita', 'layouts.medecin'], function ($view) {
+            if (auth()->check()) {
+                $user = auth()->user();
+                if ($user->isPatient()) {
+                    $view->with('upcomingCount', \App\Models\RendezVous::where('patient_id', $user->id)
+                        ->whereIn('statut', ['en_attente', 'confirme'])
+                        ->where('date_heure', '>', now())
+                        ->count());
+                } elseif ($user->isMedecin() && $user->medecin) {
+                    $view->with('todayRdvCount', \App\Models\RendezVous::where('medecin_id', $user->medecin->id)
+                        ->whereDate('date_heure', now())
+                        ->count());
+                    $view->with('pendingUrgenceCount', \App\Models\Urgence::where('statut', 'en_cours')->count());
+                }
+            }
+        });
     }
 }
